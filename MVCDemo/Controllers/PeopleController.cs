467453516257c28baf2cdfb8c39bc255f7629147
William Bunin <snowyglaze33@gmail.com>
2020-04-06 11:34:24 -0500

    Create project following Youtube tutorial

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "William", LastName = "Bunin", Age = 27, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "Leah", LastName = "Liberman", Age = 27, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Corey", Age = 38 });
+            people.Add(new PersonModel { FirstName = "JOhn", LastName = "Doe", Age = 98, IsAlive = false });
+
+            return View(people);
+        }
+    }
+}
